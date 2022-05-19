3875967ebb435525d76817ff2a9279117380a243
itsbhupendrasingh <45242141+itsbhupendrasingh@users.noreply.github.com>
2022-05-19 15:05:48 +0530

    Update eeprom_clear_NodeMCU_ESP32_Arduino.ino

--- eeprom_clear_NodeMCU_ESP32_Arduino/eeprom_clear_NodeMCU_ESP32_Arduino.ino
+++ eeprom_clear_NodeMCU_ESP32_Arduino/eeprom_clear_NodeMCU_ESP32_Arduino.ino
@@ -7,17 +7,16 @@
 */
 
 #include <EEPROM.h>
-int val = 4096; // if Arduino UNO then replace it with 512
+int val = 4096; // write a 0 to all 4096 bytes of the EEPROM
+// if Arduino UNO then replace it with 512
 void setup() {
   Serial.begin(9600);
   EEPROM.begin(val); 
-  // write a 0 to all 512 bytes of the EEPROM
   for (int i = 0; i < val; i++) {
     EEPROM.write(i, 0);
     Serial.println(i);
   }
 
-  // turn the LED on when we're done
   EEPROM.end();
   Serial.println("EEPROM Clear");
 }
